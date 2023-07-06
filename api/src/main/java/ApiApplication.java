a177ead81568201ad359ca2027ec3b461fbb2a40
eeun95 <eeun95@gmail.com>
2023-07-06 23:45:45 +0900

    모듈 분리

---
+++ api/src/main/java/ApiApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.lezhin.domain.entity")
+@EnableJpaRepositories(basePackages = "com.lezhin.domain.repository")
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
