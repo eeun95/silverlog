668364925d7251f3256d7cd9b938dd6655ca5e38
eeun95 <eeun95@gmail.com>
2023-07-19 06:02:22 +0900

    Post Controller

--- api/src/main/java/org/study/api/service/CommentServiceImpl.java
+++ api/src/main/java/org/study/api/service/CommentServiceImpl.java
@@ -1,7 +1,11 @@
 package org.study.api.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.study.repository.CommentRepository;
 
 @Service
+@RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService{
+    private final CommentRepository commentRepository;
 }
--- domain/src/main/java/org/study/repository/MemberRepository.java
+++ domain/src/main/java/org/study/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package org.study.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.study.domain.Member;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByMemberId(String memberId);
 }
